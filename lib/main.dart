afa274e3408215d3eabf46e770c65ccbfe86abda
Baptiste Marcon <baptistemcn@icloud.com>
2021-08-23 16:36:32 +0200

    feat: SearchSection

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,19 @@ import 'package:google_fonts/google_fonts.dart';
 const global_green = Color(0xFF54D3C2);
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Hotels Booking',
-        home: HomePage());
+      debugShowCheckedModeBanner: false,
+      title: 'Hotels Booking',
+      home: HomePage(),
+    );
   }
 }
 
@@ -22,7 +25,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SearchSection(),
+            HotelSection(),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -43,7 +53,10 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       title: Text(
         'Explore',
         style: GoogleFonts.nunito(
-            color: Colors.black, fontSize: 22, fontWeight: FontWeight.w800),
+          color: Colors.black,
+          fontSize: 22,
+          fontWeight: FontWeight.w800,
+        ),
       ),
       actions: [
         IconButton(
@@ -65,6 +78,139 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       ],
       centerTitle: true,
       backgroundColor: Colors.white,
+    );
+  }
+}
+
+class SearchSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      padding: EdgeInsets.fromLTRB(10, 25, 10, 10),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade300,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'London',
+                      contentPadding: EdgeInsets.all(10),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 4,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                child: ElevatedButton(
+                  child: Icon(
+                    Icons.search,
+                    size: 26,
+                  ),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(10),
+                    primary: global_green,
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Choose Date',
+                        style: GoogleFonts.nunito(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        )),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('12 Dec - 22 Dec',
+                        style: GoogleFonts.nunito(
+                          color: Colors.black,
+                          fontSize: 17,
+                        )),
+                  ],
+                ),
+              ),
+              Container(
+                height: 40,
+                child: VerticalDivider(color: Colors.grey),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Number of Rooms',
+                        style: GoogleFonts.nunito(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        )),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('1 room - 2 Adults',
+                        style: GoogleFonts.nunito(
+                          color: Colors.black,
+                          fontSize: 17,
+                        )),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HotelSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 2000,
+      color: Colors.white,
     );
   }
 }
